c910478e80849b2e5c63fd672c4fccad4240cb79
Samuele Miotto <miotto.samuele@gmail.com>
2021-11-22 15:48:51 +0100

    get email list from api

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 var app = new Vue({
     el: '#app',
     data: {
-      
+      emailList : [],
+    },
+    mounted() {
+        const self = this;
+        for (let i = 0; i < 10; i++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(function(element){
+                if (!self.emailList.includes(element)) {
+                    self.emailList.push(element.data.response)                
+                }
+            });
+        }
+        console.log(self.emailList);
     }
   });
