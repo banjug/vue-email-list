12b4ff334c494fc92cd17275285384e83980e28a
Samuele Miotto <miotto.samuele@gmail.com>
2021-11-22 16:08:00 +0100

    fix move v-for in vue div (+comments)

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 var app = new Vue({
     el: '#app',
     data: {
+        // creo l'array vuoto per le email
       emailList : [],
     },
     mounted() {
+        // seleziono this e lo salvo in una costante per richiamare data di VUE 
         const self = this;
+        // ciclo per 10 volte 
         for (let i = 0; i < 10; i++) {
+            // ogni volta richiedo una email all'api 
             axios
             .get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then(function(element){
+                // pusho l' email nell' array 
                 self.emailList.push(element.data.response)
             });
         }
